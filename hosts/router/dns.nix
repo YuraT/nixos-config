64e7aa87abbae962af6f726ffe9e9203fd91dc38
Yuri Tatishchev <itatishch@gmail.com>
2025-06-05 15:28:36 -0700

    router: dns: enable dnssec in adguard and adjust query log rotation to every 7 days

--- hosts/router/dns.nix
+++ hosts/router/dns.nix
@@ -44,10 +44,12 @@ in
   services.adguardhome.mutableSettings = false;
   # https://github.com/AdguardTeam/Adguardhome/wiki/Configuration
   services.adguardhome.settings = {
+    querylog.interval = "168h"; # 7 days
     dns = {
       # Disable rate limit, default of 20 is too low
       # https://github.com/AdguardTeam/AdGuardHome/issues/6726
       ratelimit = 0;
+      enable_dnssec = true;
       bootstrap_dns = [ "1.1.1.1" "9.9.9.9" ];
       upstream_dns = [
         # Default upstreams
