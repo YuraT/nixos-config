4d61ec46174a0c10ded59508bc91512e49c9d6af
Yuri Tatishchev <itatishch@gmail.com>
2026-02-04 19:11:03 -0800

    desktop: add zram config

--- hosts/common-desktop.nix
+++ hosts/common-desktop.nix
@@ -14,6 +14,14 @@
     timeout = 3;
     systemd-boot.enable = lib.mkDefault true;
   };
+  boot.kernel.sysctl = {
+    # https://wiki.archlinux.org/title/Zram#Optimizing_swap_on_zram
+    "vm.swappiness" = 180;
+    "vm.watermark_boost_factor" = 0;
+    "vm.watermark_scale_factor" = 125;
+    "vm.page-cluster" = 0;
+  };
+  zramSwap.enable = true;
 
   # https://nixos.wiki/wiki/Accelerated_Video_Playback
   hardware.graphics.enable = true;
